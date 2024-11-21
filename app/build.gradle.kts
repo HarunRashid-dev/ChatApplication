5263f1ca1457192248d8eba89b804bab38c3af2b
Admin <harunrashid9159@gmail.com>
2024-11-21 09:07:13 +0530

    done 21-11-2024 9:7 am

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,11 @@
+
+
 plugins {
+
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
 }
 
 android {
@@ -40,6 +44,9 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-analytics")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/eazyalgo/chatapplication/LogIn.kt
+++ app/src/main/java/com/eazyalgo/chatapplication/LogIn.kt
@@ -1,6 +1,8 @@
 package com.eazyalgo.chatapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,8 +16,37 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.eazyalgo.chatapplication.ui.theme.ChatApplicationTheme
 
 class LogIn : ComponentActivity() {
+
+    private Lateinit var edtEmail: EditText
+    private Lateinit var edtPassword: EditText
+    private Lateinit var btnLogIn: Button
+    private Lateinit var btnSignUp: Button
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogIn = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignup.setOnClickListener {
+            val intent  = Intent( this, SignUp::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+
+
+
+
         enableEdgeToEdge()
         setContent {
             ChatApplicationTheme {
